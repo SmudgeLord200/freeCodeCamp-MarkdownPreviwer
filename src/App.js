90109f8ce05099ab4363ab5499dd3c4e3d2cf4d7
Nicole <nicoleho1314@gmail.com>
2023-10-27 12:52:07 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { FaFreeCodeCamp, FaExpandArrowsAlt, FaExpandAlt } from 'react-icons/fa';
+
+const Editor = () => {
+  return (
+    <div className='editor'>
+      <div class="toolbar">
+        <FaFreeCodeCamp />
+        <h3>Editor</h3>
+        {/* TODO add event listener for expand */}
+        <FaExpandArrowsAlt />
+      </div>
+      <textarea id="editor"/>
+    </div>
+  )
+}
+
+const Previewer = () => {
+  return (
+    <div className='preview'>
+      <div class="toolbar">
+        <FaFreeCodeCamp />
+        <h3>Previewer</h3>
+        {/* TODO add event listener for expand */}
+        <FaExpandArrowsAlt />
+      </div>
+      <div id="preview">
+        fdd
+      </div>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Editor />
+      <Previewer />
     </div>
   );
 }
